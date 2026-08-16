00b98677d612cb616ed200ebc5a7acdc2f965e3f
Nishant <onenish14@gmail.com>
2026-08-16 12:46:15 +0530

    get chat api

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import multer from 'multer';
 import { Queue } from 'bullmq';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { QdrantVectorStore } from '@langchain/qdrant';
+import OpenAI from 'openai';
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 
 const queue = new Queue('file-upload-queue', {
@@ -41,6 +47,51 @@ app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
     })
   );
   return res.json({ message: 'File Uploaded Successfully' });
+});
+
+app.get('/chat', async (req, res) => {
+  const userQuery = req.query.message;
+
+  if (typeof userQuery !== 'string' || !userQuery.trim()) {
+    return res
+      .status(400)
+      .json({ error: 'Missing required query param: message' });
+  }
+
+  const embeddings = new OpenAIEmbeddings({
+    model: 'text-embedding-3-small',
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: 'http://localhost:6333',
+      collectionName: 'langchainjs-testing',
+    }
+  );
+  const ret = vectorStore.asRetriever({
+    k: 2,
+  });
+  const result = await ret.invoke(userQuery);
+
+  const SYSTEM_PROMPT = `
+  You are helfull AI Assistant who answeres the user query based on the available context from PDF File.
+  Context:
+  ${JSON.stringify(result)}
+  `;
+
+  const chatResult = await client.chat.completions.create({
+    model: 'gpt-4.1',
+    messages: [
+      { role: 'system', content: SYSTEM_PROMPT },
+      { role: 'user', content: userQuery },
+    ],
+  });
+
+  return res.json({
+    message: chatResult.choices[0].message.content,
+    docs: result,
+  });
 });
 
 
