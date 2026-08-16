72e1c3ff3f3d008ecde2663d759856ecf763980c
Nishant <onenish14@gmail.com>
2026-08-16 12:52:23 +0530

    chat page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import FileUploadComponent from "./components/file-upload";
+import ChatComponent from "./components/chat";
 
 export default function Home() {
   return (
@@ -7,8 +8,7 @@ export default function Home() {
       <div className="min-h-screen w-screen flex">
         <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center"> <FileUploadComponent /> </div>
         <div className="w-[70vw] min-h-screen border-l-2">
-          {/* <ChatComponent /> */}
-          hey
+          <ChatComponent />
         </div>
       </div>
     </div>
