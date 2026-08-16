3c7b541bda6558617178afb7a44536f8510ca752
Nishant <onenish14@gmail.com>
2026-08-16 12:25:23 +0530

    worker, docker, bullmq

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer';
+import { Queue } from 'bullmq';
+
+
+
+const queue = new Queue('file-upload-queue', {
+  connection: {
+    host: 'localhost',
+    port: '6379',
+  },
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,15 +32,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
-//   await queue.add(
-//     'file-ready',
-//     JSON.stringify({
-//       filename: req.file.originalname,
-//       destination: req.file.destination,
-//       path: req.file.path,
-//     })
-//   );
+  await queue.add(
+    'file-ready',
+    JSON.stringify({
+      filename: req.file.originalname,
+      destination: req.file.destination,
+      path: req.file.path,
+    })
+  );
   return res.json({ message: 'File Uploaded Successfully' });
 });
+
 
 app.listen(8000, () => console.log(`Server started on PORT:${8000}`))
